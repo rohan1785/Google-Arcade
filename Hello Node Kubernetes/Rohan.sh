7c54d3f1666ce063c35523e5a4f5e176a0f0ab3e
Rohan Umbarepatil <209171135+rohan1785@users.noreply.github.com>
2026-02-21 15:32:56 +0530

    Add script for deploying Node.js app on GCP with Kubernetes
    This script sets up a Node.js application in Google Cloud, builds a Docker image, and deploys it to a Kubernetes cluster.

---
+++ Hello Node Kubernetes/Rohan.sh
@@ -0,0 +1,79 @@
+
+gcloud auth list
+
+export ZONE=$(gcloud compute project-info describe --format="value(commonInstanceMetadata.items[google-compute-default-zone])")
+
+export REGION=$(gcloud compute project-info describe --format="value(commonInstanceMetadata.items[google-compute-default-region])")
+
+export PROJECT_ID=$(gcloud config get-value project)
+
+gcloud config set compute/zone "$ZONE"
+
+gcloud config set compute/region "$REGION"
+
+cat > server.js <<EOF_CP
+var http = require('http');
+var handleRequest = function(request, response) {
+  response.writeHead(200);
+  response.end("Hello World!");
+}
+var www = http.createServer(handleRequest);
+www.listen(8080);
+EOF_CP
+
+cat > Dockerfile <<EOF_CP
+FROM node:6.9.2
+EXPOSE 8080
+COPY server.js .
+CMD node server.js
+EOF_CP
+
+docker build -t gcr.io/$PROJECT_ID/hello-node:v1 .
+
+docker run -d -p 8080:8080 gcr.io/$PROJECT_ID/hello-node:v1
+
+curl http://localhost:8080
+
+ID=$(docker ps --format '{{.ID}}')
+
+docker stop $ID
+
+gcloud auth configure-docker --quiet
+
+docker push gcr.io/$PROJECT_ID/hello-node:v1
+
+gcloud config set project $PROJECT_ID
+
+gcloud container clusters create hello-world --zone="$ZONE" --num-nodes 2 --machine-type n1-standard-1
+
+kubectl create deployment hello-node --image=gcr.io/$PROJECT_ID/hello-node:v1
+
+sleep 5
+
+kubectl get deployments
+
+sleep 5
+
+kubectl get pods
+
+kubectl cluster-info
+
+kubectl config view
+
+kubectl get events
+
+kubectl expose deployment hello-node --type="LoadBalancer" --port=8080
+
+sleep 7
+
+kubectl get services
+
+kubectl scale deployment hello-node --replicas=4
+
+sleep 5
+
+kubectl get deployment
+
+sleep 7
+
+kubectl get pods
